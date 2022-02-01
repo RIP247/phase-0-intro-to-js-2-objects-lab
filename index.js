67fc2e66df4d4865dd8ae3b5f78eb71047a824e3
Rodney <rodneypa@LAPTOP-KQJ5G51E.localdomain>
2022-02-01 13:48:31 -0500

    commit tree 998e3896521fe703c0080685599786b68a9e9f83

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const employee = {
+name: { 
+  name1: "Rodney",
+  name2: "Anthony",
+  name3: "Sarah"},
+streetAddress: {
+  line1: "Trotman",
+  line2: "Himrod",
+  line3: "Central",
+},
+};
+
+function updateEmployeeWithKeyAndValue(employee,name, value) {
+ const newEmployee = {...employee};
+ newEmployee[name] = value;
+ return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,name, value){
+   employee[name] = value;
+   return employee;
+};
+
+function deleteFromEmployeeByKey(employee,name,key) {
+    const newEmployeeList = {...employee};
+    if (employee.name === employee.name) {
+        delete newEmployeeList.name;
+        return newEmployeeList;
+    };
+    newEmployeeList[name] = value;
+    return newEmployeeList;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee,name, key){
+    if (employee.key === employee.key) {
+        delete employee.name;
+        return employee;
+    };
+employee[name] = value;
+return employee;
+};
